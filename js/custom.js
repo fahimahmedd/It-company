0922f505718bacec0998b5eb7c3872351d0da48a
FNReact <commongithub@gmail.com>
2024-01-04 18:40:35 +0600

    Course-Pages-Added

--- js/custom.js
+++ js/custom.js
@@ -1,124 +1,145 @@
 $(function () {
+  // Counter
 
-  var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    speed: 1200,
-    cssEase: 'linear',
+
+  // Course-Category-Slider
+  var swiper = new Swiper(".courseCategorySlider", {
+    slidesPerView: "6",
+    spaceBetween: 18,
     loop: true,
+    // centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
     // },
+    // breakpoints: {
+    //   240: {
+    //     slidesPerView: 1,
+    //     spaceBetween: 20,
+    //   },
+    //   768: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 40,
+    //   },
+    //   1024: {
+    //     slidesPerView: 5,
+    //     spaceBetween: 50,
+    //   },
+    // },
+  });
+  // Course-Slider
+  var swiper = new Swiper(".courseItemSlider", {
+    slidesPerView: "3",
+    spaceBetween: 20,
+    loop: true,
+    // centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  });
+
+
+  var swiper = new Swiper(".reviewSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 22,
+    centeredSlides: true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      240: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: "auto",
+        spaceBetween: 20,
+      },
+    },
     autoplay: {
-      delay: 3500,
+      delay: 0,
+      pauseOnMouseEnter: true,
       disableOnInteraction: false,
-    },
-  });
-
-  // Gallery
-  var swiper = new Swiper(".mySwiperGallery", {
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    // centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      240: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 2,
-        spaceBetween: 50,
-      },
-    },
+      reverseDirection: false,      
+  },
+  speed: 8000,
   });
 
 
-  // amenities
-  var swiper = new Swiper(".mySwiperAmenities", {
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    // centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      240: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 2,
-        spaceBetween: 50,
-      },
-    },
-  });
-
-  // Back to Top 
-  var btn = $('.backtotop');
+  // Back to Top
+  var btn = $("#button");
   $(window).scroll(function () {
     if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
+      btn.addClass("show");
     } else {
-      btn.removeClass('show');
+      btn.removeClass("show");
     }
   });
 
-  btn.on('click', function (e) {
+  btn.on("click", function (e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '300');
+    $("html, body").animate({ scrollTop: 0 }, "300");
   });
 
-  $(window).scroll(function(){
-    if ($(this).scrollTop() > 150) {
-       $('.header').addClass('header_sticky');
+  // Header Fixed
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 20) {
+      $(".header").addClass("header-sticky");
     } else {
-       $('.header').removeClass('header_sticky');
+      $(".header").removeClass("header-sticky");
     }
   });
+
+
+  // Course Overview Fixed
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".course-details-sticky-wrapper").addClass("course-details-sticky-wrapper-fixed");
+    } else {
+      $(".course-details-sticky-wrapper").removeClass("course-details-sticky-wrapper-fixed");
+    }
+  });
+  
 });
 
 // Aos Animation
 AOS.init();
 
-
-// lighBox 
+// lighBox
 lightbox.option({
-  'resizeDuration': 200,
-  'wrapAround': true
-})
+  resizeDuration: 200,
+  wrapAround: true,
+});
 
 // VenuBox
 new VenoBox({
-  selector: '.my-video-links',
+  selector: ".my-video-links",
 });
 
-// Loader 
-$(window).on("load", function(){
-  $('#loading-screen').fadeOut(500);    
+// Loader
+$(window).on("load", function () {
+  $("#loading-screen").fadeOut(500);
 });
